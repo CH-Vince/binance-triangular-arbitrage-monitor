05b9242a8e64626ff01c78775d06f9c861603b15
Vince <vince@veighnsche.com>
2022-01-08 14:12:37 +0100

    ghpages

---
+++ gh-pages.js
@@ -0,0 +1,16 @@
+import ghpages from 'gh-pages'
+
+ghpages.publish(
+  'public', // path to public directory
+  {
+    branch: 'main',
+    repo: 'https://github.com/CH-Vince/binance-triangular-arbitrage-monitor.git', // Update to point to your repository
+    user: {
+      name: 'Vince Liem', // update to use your name
+      email: 'vincepaul.liem@cryptohopper.com' // Update to use your email
+    }
+  },
+  () => {
+    console.log('Deploy Complete!')
+  }
+)
